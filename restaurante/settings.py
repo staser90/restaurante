55e9ed198235a573aa7b49fcea17b2beb35b3475
staser90 <sergio8545@hotmail.com>
2026-03-17 15:47:50 +0000

    fix hotsname

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    "restaurantecafecriolo.com",
-    "www.restaurantecafecriolo.com",
-    ".onrender.com",
-]
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='restaurantecafecriolo.com,www.restaurantecafecriolo.com,.onrender.com',
+    cast=Csv()
+)
 
 
 DATABASES = {
