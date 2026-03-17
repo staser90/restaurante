13e6a0c0fc28423955f4bb3695e10b5c459a283c
staser90 <sergio8545@hotmail.com>
2026-03-17 15:31:58 +0000

    fix allowed hosts

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -26,7 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+ALLOWED_HOSTS = [
+    "restaurantecafecriolo.com",
+    "www.restaurantecafecriolo.com",
+    ".onrender.com",
+]
 
 
 DATABASES = {
